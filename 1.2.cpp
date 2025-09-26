888abd8f2556f2a5f543808842dd1aee21dcbf1e
RudDavid <satorugodze17@gmail.com>
2025-09-26 17:49:23 +0700

    Update 1.2.cpp

--- 1.2.cpp
+++ 1.2.cpp
@@ -10,36 +10,39 @@
 using namespace std;
 
 int main() {
-  const double PI = 3.141592;                           // Вводим число пи
-  const double electricConstant = 8.85 / 100;           // Электрическая постоянная
+  // Вводим число пи
+  const double PI = 3.141592;
+  // Электрическая постоянная
+  const double electricConstant = 8.85 / 100;           
 
-  int eshka;                                              // Диэлектрическая проницаемость стекла 
-  float innerCylinderRadius;                          // Радиус внутреннего цилиндра
-  float outerCylinderRadius;                                              // Радиус внешнего цилиндра
-  float lenght;                                              // Высота цилиндров
+  // Диэлектрическая проницаемость стекла 
+  int eshka;
+  // Радиус внутреннего цилиндра
+  float innerCylinderRadius;
+  // Радиус внешнего цилиндра
+  float outerCylinderRadius;
+  // Высота цилиндров
+  float lenght;                                              
   float ceshka;
 
   eshka = 6;
-  lenght = 13;
+  lenght = 13.0;
   outerCylinderRadius = 4.7;
   innerCylinderRadius = 4.5;
 
   while (outerCylinderRadius <= 5) {
-    ceshka = (2 * PI * eshka * electricConstant * lenght) / (log(outerCylinderRadius / innerCylinderRadius));
-    cout << "R = ";
-    cout << outerCylinderRadius;
-    cout << "\t\tC = ";
-    cout << ceshka << endl;
+    ceshka = (2.0f * PI * eshka * electricConstant * lenght) / (log(outerCylinderRadius / innerCylinderRadius));
+    cout << "R = " << outerCylinderRadius;
+    cout << "\t\tC = " << ceshka << endl;
     outerCylinderRadius += 0.1;
     }
     outerCylinderRadius += 0.9;
-    while (outerCylinderRadius <= 10) {
+  
+    do {
       ceshka = (2 * PI * eshka * electricConstant * lenght) / (log(outerCylinderRadius / innerCylinderRadius));
-      cout << "R = ";
-      cout << outerCylinderRadius;
-      cout << "\t\tC = ";
-      cout << ceshka << endl;
+      cout << "R = " << outerCylinderRadius;
+      cout << "\t\tC = " << ceshka << endl;
       outerCylinderRadius += 1;
-    }
-    return 0;
+    } while (outerCylinderRadius <= 10);
+  return 0;
 }
